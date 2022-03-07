03f0e399e323b5c676ad7be07aed546d3c38aadd
Abigail Turriza <abigailturriza3@gmail.com>
2022-03-06 22:43:37 -0500

    Commit Actividad B

--- app/src/main/java/com/example/cicloappnavegacion/ActividadB.kt
+++ app/src/main/java/com/example/cicloappnavegacion/ActividadB.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-
+//Actividad B
 class ActividadB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
